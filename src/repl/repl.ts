46d23cff19fb88512537e18d421abc65955e4b7c
dim-anis <anis.dim@gmail.com>
2023-08-31 17:54:31 +0400

    fix infinite loop in repl

--- src/repl/repl.ts
+++ src/repl/repl.ts
@@ -2,18 +2,35 @@ import { Lexer } from "../lexer/lexer";
 import * as readline from "node:readline/promises";
 import { stdin as input, stdout as output } from "node:process";
 
-const rl = readline.createInterface({
-  input,
-  output
-});
-
-rl.setPrompt(">> ");
 
 async function main() {
-  await rl.question("Welcome to TS version of Monkey Interpreter!")
+  const rl = readline.createInterface({
+    input,
+    output,
+  });
+
+  console.log(`Welcome to TS version of Monkey Language!
+
+Type some legal Monkey statements, 
+but be careful not to launch your machine into an infinite loop as this is a WIP!`)
+
+  rl.setPrompt(">> ");
+  rl.prompt();
 
   rl.on('line', async (input) => {
     const l = new Lexer(input);
+
+    while (true) {
+      const token = l.nextToken();
+      console.log(token);
+      if (token.type === '') {
+        break;
+      }
+    }
+  });
+
+  rl.on('close', () => {
+    console.log('Session ended')
   })
 }
 
